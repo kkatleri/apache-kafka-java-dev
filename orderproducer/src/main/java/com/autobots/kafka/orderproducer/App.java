0ec80bdfb2350703198f52b3f63ed75ff871c9d1
kkatleri <khujema.katleri@gmail.com>
2023-06-01 09:50:42 -0500

    Apache Kafka example with Java main and custom serializer

---
+++ orderproducer/src/main/java/com/autobots/kafka/orderproducer/App.java
@@ -0,0 +1,13 @@
+package com.autobots.kafka.orderproducer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
